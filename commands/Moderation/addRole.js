f2a7dfb0015bef2a6d1f43579a3b372a7dbb08bf
Donald Jennings <48395813+DonaldJennings@users.noreply.github.com>
2020-06-06 18:47:10 +0100

    Limitations on Volume

--- commands/Moderation/addRole.js
+++ commands/Moderation/addRole.js
@@ -11,8 +11,7 @@ module.exports = {
             var roleMember = message.mentions.members.first();
             if (!mentionedRole) return message.reply(`I am unable to find role: ${mentionedRole}`);
 
-
-            //roleMember.roles.add(mentionedRole).then(message.channel.send("Role successfully added.")).catch(console.error);
+           roleMember.roles.add(mentionedRole).then(message.channel.send("Role successfully added.")).catch(console.error);
         } else {
             message.reply('You do not have permission to do this')
         }
--- commands/Music/volume.js
+++ commands/Music/volume.js
@@ -4,16 +4,33 @@ module.exports = {
     cooldown: 5,
     execute(message, args) {
 
+
         if (args) {
-            const { channel } = message.member.voice;
-            if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-            const serverQueue = message.client.queue.get(message.guild.id);
-            if (!serverQueue) return message.channel.send('There is nothing playing.');
-            oldVolume = serverQueue.volume
-            if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
-            serverQueue.volume = args[0]; // eslint-disable-line
-            serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-            return message.channel.send(`Volume has been set from **${oldVolume}** to: **${args[0]}**`);
+
+            volString = args.join('')
+
+            var volInt = parseInt(volString, 10)
+
+            if (!isNaN(volInt)) {
+
+                if (volInt > 999) return message.channel.send("Volume cannot be set above 999")
+                console.log(`volInt: ${volInt}`)
+                const { channel } = message.member.voice;
+                if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+                const serverQueue = message.client.queue.get(message.guild.id);
+                if (!serverQueue) return message.channel.send('There is nothing playing.');
+                oldVolume = serverQueue.volume
+                if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
+                serverQueue.volume = volInt; // eslint-disable-line
+                serverQueue.connection.dispatcher.setVolumeLogarithmic(volInt / 5);
+                return message.channel.send(`Volume has been set from **${oldVolume}** to: **${volInt}**`);
+
+            } else {
+
+                return message.channel.send("Volume command only accepts Numerical input")
+
+            }
+            
         }
     }
 };
