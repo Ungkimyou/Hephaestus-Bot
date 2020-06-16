1d0c791a8313e76d41a18b104f1cfc17af1a614c
Donald Jennings <48395813+DonaldJennings@users.noreply.github.com>
2020-06-16 22:14:55 +0100

    Added Lyrics Command

---
+++ commands/Music/lyrics.js
@@ -0,0 +1,81 @@
+
+const cheerio = require('cheerio')
+const Genius = require('genius-api')
+const fetch = require('node-fetch')
+const {MessageEmbed} = require('discord.js')
+const genius = new Genius(process.env.GENIUS_KEY)
+const colours = require('../../colours.json')
+module.exports = {
+    name: 'lyrics',
+    description: 'Displays lyrics of currently playing/searched song.',
+    cooldown: 5,
+    execute(message, args) {
+
+        //Function to search the Genius API and grab the first song
+        function genius_search_song(song_string) {
+            genius.search(song_string)
+                .then(function (response) {
+
+
+                    song = response.hits[0].result
+
+                    console.log(genius_song_info)
+                    genius.getSongLyrics(song.url)
+
+                    return (genius_song_info)
+
+                })
+        }
+
+
+        //Parses the HTML for the lyrics under the lyrics class
+        function parseSongHTML(htmlText) {
+
+            try {
+
+            } catch (err) {
+                console.error(err)
+            }
+            const $ = cheerio.load(htmlText)
+            const lyrics = $(`div[class="lyrics"]`).text().trim()
+            console.log(lyrics)
+            console.log(typeof lyrics)
+            if (!lyrics) return message.channel.send("Could not obtain lyrics. Ensure you have entered the Artist")
+            if (lyrics.length > 3000) return message.channel.send("Could not provide lyrics as they were over character allowance")
+
+            return message.channel.send(lyrics, {split: true, code: true })
+        }
+
+
+        //Adds method to Genius class for getting the song lyrics
+        Genius.prototype.getSongLyrics = function getSongLyrics(geniusUrl) {
+            return fetch(geniusUrl, { method: 'GET' })
+                .then(response => {
+                    if (response.ok) return response.text()
+                    throw new Error('Could not get song URL...')
+                })
+            .then(parseSongHTML)
+        }
+
+
+        if (args.length > 0) {
+
+            searchArgs = args.join(' ')
+            console.log(`Genius searching for... ${searchArgs}`)
+            try {
+
+                genius_search_song(searchArgs)
+
+            } catch (err) {
+                console.error(err)
+            }
+
+        } else {
+
+        
+           return message.channel.send("Please supply some arguments")
+
+        }
+
+    }
+};
